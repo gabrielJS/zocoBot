6477afd1db5645c50211442cfb74723561fef7ea
gabrielJS <34225048+gabrielJS@users.noreply.github.com>
2018-12-12 17:21:27 -0300

    Update generaDB.sql

--- generaDB.sql
+++ generaDB.sql
@@ -8,12 +8,4 @@ CREATE TABLE `frases` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
-INSERT INTO frases (frase, autor) VALUES('Yo de verdad creo que salgo con ella Y me quedo', 'Geovanni Andreotti');
-INSERT INTO frases (frase, autor) VALUES('cata salazar me mando un pack, pero no lo muestro, pa que tan chato', 'Javier Vergara');
-INSERT INTO frases (frase, autor) VALUES('Yo jugaba a la gran capital solo xdd', 'Geovanni Andreotti');
-INSERT INTO frases (frase, autor) VALUES('voy a guardar este momento, si en un año pasa lo mismo con got, me van a chupar el pico todos los culiaos', 'Gabriel Jiménez');
-INSERT INTO frases (frase, autor) VALUES('y en todo caso, le dije "andai chistoso" no mas', 'Geovanni Andreotti');
-INSERT INTO frases (frase, autor) VALUES('quizá con got pase xdd', 'Gabriel Jiménez');
-INSERT INTO frases (frase, autor) VALUES('POYO CTM', 'Todos menos poyo');
-INSERT INTO frases (frase, autor) VALUES('esta toalla es muy absorvente y me gusta metermela por el ano es demaciado zabrozo', 'Gabriel Jiménez');
-INSERT INTO frases (frase, autor) VALUES('no wn pa que tan chato, y se va a dejar el pack pa el. Marako', 'Gabriel Jiménez');
+INSERT INTO frases (frase, autor) VALUES('Test', 'User Test');
